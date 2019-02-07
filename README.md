4a9e2499745cb335eeee0a5faa6aa3f857ef470f
Max Rasmusen <maxrasmusen11742@gmail.com>
2019-02-07 17:32:16 +0000

    added pdf handling

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # suade-reports
+
+## Requirements
+- Run the command `pip install -r requirements.txt` from the root directory.
+- You need to install [wkhtmltopdf|https://wkhtmltopdf.org/downloads.html]
+- You must add the suade-reports directory to your PATH
+
+## To run
+- Navigate to the root of the project and run `FLASK_APP=app.py flask run`
--- resources/reports_resource.py
+++ resources/reports_resource.py
@@ -3,11 +3,23 @@
 """
 
 from database.reports_database import NoDatabaseError, ResourceNotFoundError, MalformedDataError
+from flask import render_template, send_file
 import dicttoxml
+import os
+import pdfkit
 
 class ReportsResource():
   def __init__(self, database):
     self.db = database
+    self.counter = 0 # counter increments so we can be sure to not rename files
+    self.pdf_outdir = os.path.join(os.path.dirname(__file__), "temp_pdfs")
+
+    # Clear pdfs directory
+    for the_file in os.listdir(self.pdf_outdir):
+      file_path = os.path.join(self.pdf_outdir, the_file)
+      if os.path.isfile(file_path):
+          os.unlink(file_path)
+
   
   def get_report(self, request_args, report_id):
     try:
@@ -21,7 +33,20 @@ class ReportsResource():
         xml = dicttoxml.dicttoxml(data)
         return (xml, 200, {"Content-Type": "text/xml"})  
       else:
-        return ("OK", 200)
+        html = render_template("pdf_template.html",
+                               org_name=data["organization"],
+                               reported_time=data["reported_at"],
+                               created_time=data["created_at"],
+                               inventory=data["inventory"])
+
+        self.counter += 1
+        pdffilename = data["organization"] + "-" + str(self.counter) + ".pdf"
+        pdfout = os.path.join(self.pdf_outdir, pdffilename)
+
+        pdfkit.from_string(html, pdfout)
+                                       
+        # return (pdfout, 200, {"Content-Type": "application/pdf", "Content-Disposition": "inline;filename=" + pdffilename})
+        return send_file(pdfout)
       
     except NoDatabaseError:
       return ("No connection to database.", 503)
